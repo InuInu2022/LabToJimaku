72451d7189171fce4b0369357d6d99f05343b8d0
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-12-08 19:26:26 +0900

    公開用

---
+++ LabToJimaku/Program.cs
@@ -0,0 +1,53 @@
+﻿using LibSasara;
+using WanaKanaNet;
+
+//コマンドライン引数の一つ目はlabファイルへのパス
+var labPath = args[0];
+
+//Labファイル読み込み
+var lab = await SasaraLabel.LoadAsync(labPath);
+
+//Console.WriteLine(lab.ToString());
+
+//音素からカナ変換の特別ルール
+WanaKanaOptions kanaOption = new()
+{
+	CustomKanaMapping = new Dictionary<string, string>()
+	{
+		{"cl","っ"},
+		{"di","でぃ"}
+	}
+};
+
+var output = lab
+	//小節・フレーズ単位で分割（0.1秒空白毎）
+	.SplitToSentence(0.1)
+	//必要な情報だけ抜き出してリスト化
+	.Select(v => (
+		Start: v[0].From / 10000000,    //開始
+		End: v[^1].To / 10000000,   //終了
+		Phrase: string.Join("", v.Select(s => s.Phoneme))   //音素文字列
+	))
+	//音素 -> ひらがな変換
+	.Select(v => (
+		Start: v.Start,
+		End: v.End,
+		Phrase: WanaKana.ToHiragana(v.Phrase, kanaOption)
+	))
+	//字幕(.srt)フォーマットに変換
+	.Select((v,i)=> $"""
+{i+1}
+{TimeSpan.FromSeconds(v.Start).ToString(@"hh\:mm\:ss\,fff")} --> {TimeSpan.FromSeconds(v.End).ToString(@"hh\:mm\:ss\,fff")}
+{v.Phrase}
+
+"""
+	)
+	;
+
+//labファイルと同じ名前で拡張子を.srtに変換して保存
+await File.WriteAllLinesAsync(
+	Path.ChangeExtension(labPath, "srt"),
+	output
+);
+
+//Console.WriteLine(string.Join(Environment.NewLine, output));
